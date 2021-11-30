7ddca371b3d711ee134c2f0370a27c0b8d3f0bf8
Ryszard <harrtuck@gmail.com>
2021-12-01 00:39:57 +0100

    redisign

--- lostpet/src/components/Header.js
+++ lostpet/src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import GoogleAuth from "./GoogleAuth";
 import logo from "../images/pets.png";
+import header from "./header.css";
 const Header = () => {
   const divStyle = {
     borderBottom: "1px solid black",
@@ -9,24 +10,27 @@ const Header = () => {
   };
   const groupStyle = {};
   const itempadd = {
-    height:"20%",
+    height:"10px",
     marginTop: "5px ",
     marginRight: "10px",
   };
   return (
     <div style={divStyle}>
       <div>
+      <Link to="/" className="item">
         <img className="ui small centered image" src={logo}></img>
+        </Link>
       </div>
       <div style={groupStyle}>
         <div className="ui secondary pointing menu">
           <Link to="/NewAd" className="item">
-            <button className="ui black button">Create new ad</button>
+            <button className="ui black button   headerBtn">Create new ad</button>
           </Link>
           <Link to="/AdList" className="ui item">
-            <button className="ui black button">Show list of ads</button>
+            <button className="ui black button headerBtn">Show list of ads</button>
           </Link>
           <div className="right menu " style={itempadd}>
+            
             <GoogleAuth />
           </div>
         </div>
